2198a592a3bca5981d69e1f155ae1729ae156d91
louay nahdi <paladinx@live.fr>
2021-03-30 08:24:54 +0200

    classifier module set _ testing tokenization

---
+++ nltkEnable.py
@@ -0,0 +1,13 @@
+# The nltk download library can be buggy sometimes so i added this script to enable its ssl certificate and let it download.
+
+import nltk
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download()
